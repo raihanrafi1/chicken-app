4c33365947418c43a11e856e86530d5a4530779b
Raihan <raihan@example.com>
2026-01-09 19:57:45 +0700

    Fix: Remove standalone output to resolve 404 on Vercel

--- next.config.js
+++ next.config.js
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'standalone',
-    eslint: {
-        ignoreDuringBuilds: true,
-    },
+    /* config options here */
 }
 
 module.exports = nextConfig
